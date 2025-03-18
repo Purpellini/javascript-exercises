06e38ea360db2531cc651f14d635f20b0245b371
Purpellini <purpel@PC.>
2025-03-18 15:43:18 +0100

    1.0

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let FtoC = (fahrenheit-32)*(5/9)
+  FtoC = parseFloat(FtoC.toFixed(1))
+  console.log(FtoC)
+  return FtoC
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+  let CtoF = (Celsius*(9/5))+32
+  console.log(CtoF)
+  return CtoF
+
 };
 
+
+
+convertToCelsius(32) // fahrenheit to celsius, should return 0
+convertToCelsius(100) 
+
+convertToFahrenheit(0) // celsius to fahrenheit, should return 32
 // Do not edit below this line
+
+/* Number. prototype. toFixed()  */
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
